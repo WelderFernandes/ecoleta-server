b11a04a4bdb63c51799a1bc39ce6665ad55d778b
Welder n. fernandes <welderx3@gmaiil.com>
2020-07-04 16:08:55 -0300

    Config server

--- src/server.ts
+++ src/server.ts
@@ -11,5 +11,15 @@ app.use(routes)
 
 app.use('/uploads', express.static(path.resolve(__dirname,'..','uploads')))
 
+if( process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+
+    const path = require('path')
+    app.get('*', function (req,res) {
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
+
 app.use(errors());
-app.listen(process.env.PORT || 3333) 
+const PORT = process.env.PORT || 8000;
+app.listen(PORT) 
